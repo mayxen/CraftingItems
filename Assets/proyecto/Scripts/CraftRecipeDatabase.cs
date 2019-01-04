f35e7df7a3b4147fbbb24924889c2deeef9b7351
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-01-04 22:01:45 +0000

    change 1
    just create a few scripts

---
+++ Assets/proyecto/Scripts/CraftRecipeDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CraftRecipeDatabase : MonoBehaviour
+{
+    public List<CraftRecipe> Recipes { get; set; }
+    ItemDatabase itemDatabase;
+
+    private void Awake()
+    {
+        itemDatabase = GetComponent<ItemDatabase>();
+        BuildCraftRecipeDatabase();
+    }
+
+    public Item CheckRecipe(int[] recipe)
+    {
+        CraftRecipe targetRecipe = Recipes.Find(i => i.requiredItems.SequenceEqual(recipe));
+        return itemDatabase.GetItem(targetRecipe.itemToCraft);
+    }
+
+    void BuildCraftRecipeDatabase()
+    {
+        Recipes = new List<CraftRecipe>()
+        {
+            new CraftRecipe(1,new int[]
+            {
+                0,0,0,
+                0,2,0,
+                0,0,0
+            }),
+            new CraftRecipe(2,new int[]
+            {
+                3,0,0,
+                0,3,0,
+                0,0,3
+            }),
+            new CraftRecipe(3,new int[]
+            {
+                1,0,2,
+                0,2,0,
+                0,0,0
+            }),
+        };
+    }
+}
---
+++ Assets/proyecto/Scripts/ItemDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> Items { get; set; }
+
+    void Awake()
+    {
+        BuildItemDatabase();   
+    }
+
+    public Item GetItem(int id)
+    {
+        return Items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string tittle)
+    {
+        return Items.Find(item => item.title == tittle);
+    }
+
+    void BuildItemDatabase()
+    {
+        Items = new List<Item>()
+        {
+            new Item(1,"Diamont Sword","A sword made of diamont",
+                new Dictionary<string, int>()
+                {
+                    { "Power",15},
+                    { "Defence",15}
+                }),
+            new Item(2,"Diamont Ore","A shiny diamond",
+                new Dictionary<string, int>()
+                {
+                    { "Value",2500},
+                }),
+            new Item(3,"Steel Sword","A sword made of Steel",
+                new Dictionary<string, int>()
+                {
+                    { "Power",8},
+                    { "Defence",8}
+                }),
+        };
+    }
+}
